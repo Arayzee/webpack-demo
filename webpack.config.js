74516f6852259803909e0ee524d3f35675e80644
Arayze <arayzee@163.com>
2020-08-25 11:45:37 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'index.[contenthash].js'
+        filename: '[name].[contenthash].js'
     },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Webpack Test',
-        template: 'src/assets/test.html'
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Test',
+            template: 'src/assets/test.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+            ignoreOrder: false
+        }),
+    ],
     devServer: {
         contentBase: './dist',
     },
@@ -19,7 +27,13 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../',
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
                     'css-loader'
                 ]
             }
