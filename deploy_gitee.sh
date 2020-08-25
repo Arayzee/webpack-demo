e018c111472c6a5d85d6a2c565662f16923fb561
Arayze <arayzee@163.com>
2020-08-25 21:25:42 +0800

    update

---
+++ deploy_gitee.sh
@@ -0,0 +1,9 @@
+yarn build &&
+git checkout gh-pages &&
+rm -rf *.js *.css *.html *.jpg *.png *.sh *.json yarn.lock &&
+mv dist/* ./ &&
+rm -rf dist;
+git add . &&
+git commit -m "update" &&
+git push gitee gh-pages:master &&
+git checkout -
