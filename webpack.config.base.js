426376683952eb93aa5e67ba5b375a0e4e309806
Arayze <arayzee@163.com>
2020-08-25 19:40:41 +0800

    finish

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,5 +11,29 @@ module.exports = {
             title: 'Webpack Test',
             template: 'src/assets/test.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.styl$/,
+                use: ['style-loader', 'css-loader', 'stylus-loader']
+            }
+        ]
+    }
 }
